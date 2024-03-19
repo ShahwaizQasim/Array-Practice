5041ff4740457d939a6c840714ef3127e71afc03
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-03-19 10:13:41 +0500

    array

---
+++ app.js
@@ -0,0 +1,10 @@
+let colors = ["Red","Brown"];
+
+colors.push("Yellow"); // push end me value add krta hai
+//colors.unshift("Black") // unshift start se element add krta hai
+
+//colors.pop()  // pop end se element remove krta hai
+colors.shift() //shift start se element remove krta hai
+
+
+console.log(colors);
